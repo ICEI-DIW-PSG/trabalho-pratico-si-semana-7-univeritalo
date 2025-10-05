6bbf13db4ccc0c7d6b352169bc3b23a6cd4b524f
Italo Gabriel Justino Caldeira <univeritalo@gmail.com>
2025-10-05 09:40:04 -0300

    Estruturação das Páginas index e detalhes

---
+++ public/app.js
@@ -0,0 +1,50 @@
+const detalhes = [
+    {
+        "id": 1,
+        "titulo": "Conjunto Feminino",
+        "descricao": "Peça feminina (conjunto) de verão para uso diário",
+        "conteudo": "Conjunto de Top Rosa, Short Jeans Preto e Cinto Preto",
+        "categoria": "Cojunto",
+        "imagem": "img/roupa1.jpg"
+    },
+    {
+        "id": 2,
+        "titulo": "Conjunto Feminino",
+        "descricao": "Peça feminina (conjunto) de verão para uso diário",
+        "conteudo": "Conjunto de Regata Branca e Short Jeans Preto",
+        "categoria": "Cojunto",
+        "imagem": "img/roupa2.jpg"
+    },
+    {
+        "id": 3,
+        "titulo": "Conjunto Feminino",
+        "descricao": "Peça feminina (conjunto) de verão para uso diário",
+        "conteudo": "Conjunto de Top Branco e Short Jeans",
+        "categoria": "Cojunto",
+        "imagem": "img/roupa3.jpg"
+    },
+    {
+        "id": 4,
+        "titulo": "Vestido Feminino",
+        "descricao": "Peça feminina (vestido) para Festas e uso diário",
+        "conteudo": "Vestido florido na cor Rosa",
+        "categoria": "Vestido",
+        "imagem": "img/roupa4.jpg"
+    }
+  {
+        "id": 5,
+        "titulo": "Vestido Feminino",
+        "descricao": "Peça feminina (vestido) para Festas e uso diário",
+        "conteudo": "Vestido florido na cor Azul",
+        "categoria": "Vestido",
+        "imagem": "img/roupa5.jpg"
+    }
+  {
+        "id": 6,
+        "titulo": "Vestido Feminino",
+        "descricao": "Peça feminina (vestido) para Festas e uso diário",
+        "conteudo": "Vestido florido na cor Azul Serenity",
+        "categoria": "Vestido",
+        "imagem": "img/roupa6.jpg"
+    }
+]
